d70b623c1465d6f277579401ccf5007fcf9ba167
白富红 <2417948448@qq.com>
2021-04-16 17:53:07 +0800

    创建 addrbook-consumer

---
+++ addrbook-consumer/src/main/java/cn/ekgc/addrbook/controller/UserController.java
@@ -0,0 +1,10 @@
+package cn.ekgc.addrbook.controller;
+
+import cn.ekgc.addrbook.controller.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userController")
+@RequestMapping("/user")
+public class UserController extends BaseController {
+}
