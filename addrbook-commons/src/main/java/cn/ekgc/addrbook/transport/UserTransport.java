bea48703ef62c8b10d21d7f9b15ce1540e0cce8e
白富红 <2417948448@qq.com>
2021-04-16 17:52:03 +0800

    创建 addrbook-commons

---
+++ addrbook-commons/src/main/java/cn/ekgc/addrbook/transport/UserTransport.java
@@ -0,0 +1,15 @@
+package cn.ekgc.addrbook.transport;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <b>用户功能传输层接口</b>
+ * @author
+ * @version 5.0.0
+ */
+@FeignClient(name = "addrbook-provider")
+@RequestMapping("/user/trans")
+public interface UserTransport {
+
+}
